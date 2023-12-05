bc96070477a7b8211f6280d0aab598b3229ad0d5
Gabriel Martins <39447491+gabriellsux@users.noreply.github.com>
2023-12-05 11:52:55 -0300

    Projeto finalizado!!

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ document.querySelector('.busca').addEventListener('submit', async (event) =>{
         clearInfo();
         showWarning('Carregando...');
 
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=b1399564616cce4ff350e9ff83c7312a&units=metric&lang=pt_br`;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=9cffd38ea43bbf2bc472c8cfcba06120&units=metric&lang=pt_br`;
 
-        let results = await fetch(url);
-        let json = await results.json();
+        let response = await fetch(url);
+        let json = await response.json();
 
         if(json.cod === 200) {
             showInfo({
